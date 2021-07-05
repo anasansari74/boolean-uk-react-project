91ad94f4f783796b66d1849c2a83a1204860e224
Anas Ansari <anas_ansar1@outlook.com>
2021-07-06 00:56:40 +0100

    second commit

--- src/pages/MoviesPage.jsx
+++ src/pages/MoviesPage.jsx
@@ -5,49 +5,33 @@ import MovieCards from "../components/MovieCards";
 function MoviesPage() {
   return (
     <>
-      <div class="drop-down-forms">
+      <div className="drop-down-forms">
         <form acion="submit" method="get">
-          <label for="movie-genre">Choose a genre:</label>
+          <label htmlFor="movie-genre">Choose a genre:</label>
           <select name="genre" id="movie-genre">
             <option value="">All</option>
-            <option value="action">Action</option>
-            <option value="fantasy">Fantasy</option>
-            <option value="adventure">Adventure</option>
-            <option value="thriller">Thriller</option>
-            <option value="drama">Drama</option>
-            <option value="biography">Biography</option>
-            <option value="Sci-fi">Sci-Fi</option>
-            <option value="crime">Crime</option>
-            <option value="mystery">Mystery</option>
+            <option value="1">Action</option>
+            <option value="2">Fantasy</option>
+            <option value="3">Adventure</option>
+            <option value="4">Thriller</option>
+            <option value="5">Drama</option>
+            <option value="6">Biography</option>
+            <option value="7">Sci-Fi</option>
+            <option value="8">Crime</option>
+            <option value="9">Mystery</option>
           </select>
         </form>
         <form action="submit" method="get">
-          <label for="rating">Rating:</label>
+          <label htmlFor="rating">Rating:</label>
           <select name="rating" id="rating">
             <option value="">--Please choose an option--</option>
-            <option value="dog">High to Low</option>
-            <option value="cat">Low to High</option>
+            <option value="1">High to Low</option>
+            <option value="2">Low to High</option>
           </select>
         </form>
       </div>
       <ul className="cards">
         <MovieCards />
-        {/* <li className="card">
-          <div className="card-header">
-            <h1>The Imitation Game</h1>
-            <input type="checkbox" className="star-checkbox" checked="false" />
-          </div>
-          <img
-            className=""
-            width="200px"
-            height="200px"
-            src="https://images-na.ssl-images-amazon.com/images/M/MV5BOTgwMzFiMWYtZDhlNS00ODNkLWJiODAtZDVhNzgyNzJhYjQ4L2ltYWdlXkEyXkFqcGdeQXVyNzEzOTYxNTQ@._V1_UX182_CR0,0,182,268_AL_.jpg"
-            alt="movie-img"
-          />
-          <p>Rated: 4.5/5</p>
-          <p>Description: yahoo!</p>
-          <button>Rate Me!</button>
-        </li> */}
       </ul>
     </>
   );
--- src/store.js
+++ src/store.js
@@ -3,9 +3,16 @@ import create from "zustand";
 const useStore = create((set, get) => ({
   movies: [],
   fetchMovies: () => {
-    fetch("http://localhost:3000/movies")
+    fetch("http://localhost:4000/movies")
       .then((resp) => resp.json())
       .then((movie) => set({ movies: movie }));
+  },
+  getFilteredMoviesByGenres: (genreId) => {
+    const foundGenre = movieGenre.find((movGen) => genreId === movGen.id);
+    const foundMovieByGenre = movies.filter(
+      (movie) => foundGenre.movieId === movie.id
+    );
+    return foundMovieByGenre;
   },
 }));
 
